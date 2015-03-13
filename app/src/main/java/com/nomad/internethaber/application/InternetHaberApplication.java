ca5bb031d8710652fd2c9ce8fb2fb74ae37e4aac
Emre Akturk <emreaktrk@hotmail.com>
2015-03-13 19:19:33 +0200

    Typo fix.

--- app/src/main/java/com/nomad/internethaber/application/InternetHaberApplication.java
+++ app/src/main/java/com/nomad/internethaber/application/InternetHaberApplication.java
@@ -3,19 +3,19 @@ package com.nomad.internethaber.application;
 import com.nomad.internethaber.helper.ResponsiveController;
 
 public final class InternetHaberApplication extends BaseApplication {
-    private static ResponsiveController mResponsiveControler;
+    private static ResponsiveController mResponsiveController;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        mResponsiveControler = new ResponsiveController(getResources());
+        mResponsiveController = new ResponsiveController(getResources());
 
 //        PushRestInterface pushRestInterface = RestAdapterProvider.getInstance().create(PushRestInterface.class);
 //        pushRestInterface.get();
     }
 
     public static ResponsiveController getResponsiveController() {
-        return mResponsiveControler;
+        return mResponsiveController;
     }
 }
