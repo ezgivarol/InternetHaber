b9a3da25ecc72f81759b07e36130ab2849dcae58
Emre Akturk <emreaktrk@hotmail.com>
2015-03-14 09:11:25 +0200

    Retry feature added to news detail.

--- app/src/main/java/com/nomad/internethaber/activity/NewsDetailActivity.java
+++ app/src/main/java/com/nomad/internethaber/activity/NewsDetailActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -149,7 +148,17 @@ public final class NewsDetailActivity extends BaseActivity implements NewsDetail
 
     @Override
     public void onActionClicked(Snackbar snackbar) {
+        mPagerAdapter.setLoading(true);
 
+        String from = mInterval.getFrom();
+        String to = mInterval.getTo();
+        String category = mCategory.getId();
+
+        mAsyncTask = new NewsMoreAsyncTask();
+        mAsyncTask.setFrom(from);
+        mAsyncTask.setTo(to);
+        mAsyncTask.setCategoryId(category);
+        mAsyncTask.execute();
     }
 
     @Override
