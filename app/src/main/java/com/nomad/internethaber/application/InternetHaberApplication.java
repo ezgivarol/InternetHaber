1d71bcec8f744863c8824a89b5bc47af41668479
ihsan <i214b606>
2015-04-14 17:58:36 +0300

    Push integrated.

--- app/src/main/java/com/nomad/internethaber/application/InternetHaberApplication.java
+++ app/src/main/java/com/nomad/internethaber/application/InternetHaberApplication.java
@@ -1,21 +1,37 @@
 package com.nomad.internethaber.application;
 
-import com.nomad.internethaber.helper.ResponsiveController;
+import android.support.multidex.MultiDex;
 
-public final class InternetHaberApplication extends BaseApplication {
+import com.nomad.internethaber.helper.ResponsiveController;
+import com.orhanobut.logger.Logger;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
+
+public final class InternetHaberApplication extends BaseApplication implements SaveCallback {
     private static ResponsiveController mResponsiveController;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        mResponsiveController = new ResponsiveController(getResources());
+        MultiDex.install(this);
+        Parse.initialize(this, "JAuXIRsmTMHRfNUFIsaJSDBvwcqJwhXc5b0uTtTH", "hUwnXGB4B7ngkBRjUBVyAxnE3Xrz4NueT0IJtiSJ");
 
-//        PushRestInterface pushRestInterface = RestAdapterProvider.getInstance().create(PushRestInterface.class);
-//        pushRestInterface.get();
+        mResponsiveController = new ResponsiveController(getResources());
     }
 
     public static ResponsiveController getResponsiveController() {
         return mResponsiveController;
+    }
+
+    @Override
+    public void done(ParseException e) {
+        if (e == null){
+            Logger.v("Subscribed to push.");
+            return;
+        }
+
+        Logger.e(e);
     }
 }
--- app/src/main/java/com/nomad/internethaber/constant/ApplicationConstants.java
+++ app/src/main/java/com/nomad/internethaber/constant/ApplicationConstants.java
@@ -3,6 +3,8 @@ package com.nomad.internethaber.constant;
 public final class ApplicationConstants {
 
     public static final String API_URL = "http://www.internethaber.com";
+    public static final String API_ID = "JAuXIRsmTMHRfNUFIsaJSDBvwcqJwhXc5b0uTtTH";
+    public static final String CLIENT_KEY = "hUwnXGB4B7ngkBRjUBVyAxnE3Xrz4NueT0IJtiSJ";
 
     public static final int PAGE_SIZE = 20;
 
